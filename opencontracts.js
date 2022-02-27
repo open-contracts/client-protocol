5717d648a624369377fef7388e7c257c83287a50
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-27 02:15:41 -0800

    Update opencontracts.js

--- opencontracts.js
+++ opencontracts.js
@@ -574,7 +574,7 @@ async function OpenContracts() {
                         for (let i = 0; i < this.outputs.length; i++) {
                             window.f = this;
                             if (this.outputs[i].name == "") {this.outputs[i].name = this.outputs[i].type}
-                            if (this.outputs[i].decimals) {txReturn[i] = ethers.utils.parseUnits(String(txReturn[i]), this.outputs[i].decimals)}
+                            if (this.outputs[i].decimals) {txReturn[i] = ethers.utils.formatUnits(String(txReturn[i]), this.outputs[i].decimals)}
                             results += `${this.outputs.name}: ${String(txReturn[i])}`
                             if (i < this.outputs.length-1) {results += ", "}
                         }
